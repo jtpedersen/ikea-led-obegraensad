5557b190b3b1da7913eb647bdcc8909ee7b35ee1
jacob toft pedersen <jacob@rotand.dk>
2025-08-15 19:37:26 +0200

    better clean up

--- plugin_tester/run.sh
+++ plugin_tester/run.sh
@@ -2,9 +2,44 @@
 set -e
 set -x
 
-ROOT=`pwd`/..
+set -euo pipefail
 
-g++ -DHOST_DDP -I ${ROOT}/include -I . host_ddp.cpp ${ROOT}/src/plugins/Blop.cpp  -o ddp
+cleanup() {
+  echo "Cleaning up..."
+  kill "$pid"
+  wait "$pid" 2>/dev/null || true # avoid zombie and ignore exit code
+  echo "Cleaning up... and restoring display state"
+  curl -X PATCH "${SERVER}/api/plugin?id=${PLUGIN_ID}"
 
+}
 
-./ddp
+trap 'cleanup; exit 0' INT TERM
+
+ROOT=$(pwd)/..
+
+g++ -DHOST_DDP -I ${ROOT}/include -I . host_ddp.cpp ${ROOT}/src/plugins/Blop.cpp -o ddp
+
+# enable DDD plugin
+SERVER="http://192.168.1.112"
+DDP_PLUGIN_ID=17
+PLUGIN_ID=10
+
+# your cleanup commands here
+
+curl -X PATCH "${SERVER}/api/plugin?id=${DDP_PLUGIN_ID}"
+
+./ddp & pid=$!
+echo "ddp running (pid=$pid). In Emacs compilation buffer, press C-c C-c to stop."
+
+# If run interactively (not via Emacs), allow any key to stop:
+if [ -t 0 ]; then
+  read -rsn1
+  kill -- -"$pid"
+else
+  # In non-interactive mode, wait until we get a signal from Emacs
+  wait "$pid" || true
+fi
+
+cleanup
+
+echo "Ctrl-C to quit"
